1fdc24a8f8641ef7d412a764ab379942123a9933
Allan Goncalves <allangoncalves.adm@gmail.com>
2023-10-29 10:50:56 -0300

    Project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
 export function App() {
-  return <h1>Empty Project</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Empty Project</h1>
+    </ThemeProvider>
+  )
 }
