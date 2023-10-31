d020e6f9994c44983c4142bdd8f413a706ae65f9
Allan Goncalves <allangoncalves.adm@gmail.com>
2023-10-31 05:15:26 -0300

    Add Transactions Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Transaction } from './pages/Transactions'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Empty Project</h1>
+      <Transaction />
     </ThemeProvider>
   )
 }
